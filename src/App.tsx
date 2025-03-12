4e49bacf05cac24a84ae79c8182fe06984ab9a1c
Daryl <2382029@unai.edu>
2025-03-13 03:24:31 +0700

    memperbaiki

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
+
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from "react-router-dom";
 import RootLayout from "./layouts/RootLayout";
-import Carts from "./pages/Carts";
+import Carts from "./pages/Comments";
 import Post from "./pages/Post";
 import Product from "./pages/Product";
 import Recipes from "./pages/Recipes";
-
+import ProductDetail from "./pages/ProductDetail";
+import Home from "./pages/Home";
+import AddProduct from "./pages/AddProduct";
+import EditProduct from "./pages/EditProduct";
+import Todos from "./pages/Todos";
+const queryClient = new QueryClient()
 
 function App() {
 	const router = createBrowserRouter(createRoutesFromElements(
-		<Route path="/" element={<RootLayout/>}>
-			<Route index element={<Product/>}/>	
-			<Route path="recipes" element={<Recipes/>}/>	
-			<Route path="posts" element={<Post/>}/>	
-			<Route path="carts" element={<Carts/>}/>	
+		<Route path="/" element={<RootLayout />}>
+			<Route index element={<Home/>} />
+			<Route path="product" element={<Product />} />
+			<Route path="product/add" element={<AddProduct />} />
+			<Route path="product/:id" element={<ProductDetail/>}/>
+			<Route path="product/:id/edit" element={<EditProduct/>}/>
+			<Route path="recipes" element={<Recipes />} />
+			<Route path="posts" element={<Post />} />
+			<Route path="comments" element={<Carts />} />
+			<Route path="/todos" element={<Todos />} />
 		</Route>
 	));
 	return (
 		<>
-		<RouterProvider router={router}/>
+			<QueryClientProvider client={queryClient}>
+				<RouterProvider router={router} />
+			</QueryClientProvider>
 		</>
 	)
 }
